21d51ae6d8206d13a8c365250565f2305baf7d83
arkanbs <115327354+arkanbs@users.noreply.github.com>
2022-10-17 22:45:52 +0800

    Delete PRAKTIKUM MODUL 2 directory

--- PRAKTIKUM MODUL 2/Python/PRAK109-2210817210008-Bima Sanjaya.py
+++
@@ -1,5 +0,0 @@
-YZ=958730; DA=5
-print("Jumlah pasukan yang dibawa Yu Zhong = ?", YZ, "pasukan")
-print("Jumlah pahlawan = ?", DA, "pahlawan")
-J=YZ/DA
-print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah", int(J), "pasukan")
