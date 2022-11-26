a6f6e3c2536b2f472e0390d353be2cc3ba1bffb9
arkanbs <115327354+arkanbs@users.noreply.github.com>
2022-11-26 11:02:31 +0800

    Delete PRAKTIKUM MODUL 7 directory

--- PRAKTIKUM MODUL 7/SOAL 2/PRAK602-2210817210008-Bima Sanjaya.py
+++
@@ -1,5 +0,0 @@
-kolom=int(input())
-isi=[]
-m=list(map(int,input().split()))
-for i in range (kolom):
-    print((i+1)*m[i],end=' ')
--- PRAKTIKUM MODUL 7/SOAL 3/PRAK603-2210817210008-Bima Sanjaya.py
+++
@@ -1,8 +0,0 @@
-a,b=map(int,input().split())
-if (a==b):
-    b1=list(map(int,input().split()))
-    b2=list(map(int,input().split()))
-    for i in range (a):
-        print(b1[i]*b2[i],end=' ')
-else:
-    print("Jumlah tidak sama")
